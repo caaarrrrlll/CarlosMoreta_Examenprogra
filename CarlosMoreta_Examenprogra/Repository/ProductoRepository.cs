9272850894116856fec1fecb10080e1347afefc2
caaarrrrlll <carlosmoreta140@gmail.com>
2025-07-10 07:56:52 -0500

    Agregacion de las carpetas Models, Repo, ViewModels, y View asi como las respectivas clases que voy a realizar

---
+++ CarlosMoreta_Examenprogra/Repository/ProductoRepository.cs
@@ -0,0 +1,31 @@
+﻿using CarlosMoreta_Examenprogra.Models;
+using SQLite.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarlosMoreta_Examenprogra.Repository
+{
+    public class ProductoRepository
+    {
+        private readonly SQLiteConnection _connection;
+
+        public ProductoRepository(string dbPath)
+        {
+            _connection = new SQLiteConnection(dbPath);
+            _connection.CreateTable<Producto>();
+        }
+
+        public int AgregarProducto(Producto producto)
+        {
+            return _connection.Insert(producto);
+        }
+
+        public List<Producto> ObtenerProductos()
+        {
+            return _connection.Table<Producto>().ToList();
+        }
+    }
+}
