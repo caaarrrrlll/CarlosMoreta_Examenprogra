910ccd4385d3bd4e6f2eefc2d00b79d8abfc57ad
caaarrrrlll <carlosmoreta140@gmail.com>
2025-07-10 08:58:30 -0500

    Solucion de errores al momento de entrar y visualizar, asi como agregar una propiedad nombres para el log

--- CarlosMoreta_Examenprogra/LogService.cs
+++ CarlosMoreta_Examenprogra/LogService.cs
@@ -8,17 +8,19 @@ namespace CarlosMoreta_Examenprogra
 {
     public class LogService
     {
-        private readonly string _logFilePath; 
+        private readonly string _logFilePath;
+        private readonly string _nombreProducto;  
 
-        public LogService(string Apellido)
+        public LogService(string apellido, string nombreProducto)
         {
-            string filename = $"Logs_{Apellido}.txt";
-            _logFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), filename); 
+            string filename = $"Logs_{apellido}.txt";
+            _logFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), filename);
+            _nombreProducto = nombreProducto; 
         }
 
-        public void Log(string message)
+        public void EscribirLog(string message)
         {
-            string logEntry = $"{DateTime.Now:dd/MM/yyyy HH:mm} - {message}";
+            string logEntry = $"Se incluyo el registro de [{_nombreProducto}] el {DateTime.Now:dd/MM/yyyy HH:mm} - {message}";
             File.AppendAllText(_logFilePath, logEntry + Environment.NewLine);
         }
 
